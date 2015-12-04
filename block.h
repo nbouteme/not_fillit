523a7ffbb13fc67f698b3d05d7fe2f36f9266a85
Nabil BOUTEMEUR <nbouteme@student.42.fr>
2015-12-04 11:44:45 +0100

    Oublie de fichier precedent commit

--- block.h
+++ block.h
@@ -6,7 +6,7 @@
 /*   By: nbouteme <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/02 10:07:20 by nbouteme          #+#    #+#             */
-/*   Updated: 2015/12/03 14:59:14 by nbouteme         ###   ########.fr       */
+/*   Updated: 2015/12/04 11:43:47 by nbouteme         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,12 @@
 
 # include "render.h"
 
-unsigned short	recognize(char **block);
-unsigned short	translate(char **block, int xshift, int yshift);
-char			**chunk(char **block, t_point pos);
-void			set_chunk(char **block, char **chunk, t_point pos);
-void			merge_chunk(char **tet, char **cur);
-int				col_empty(char **block, int l, int n);
-int				line_empty(char **block, int col, int n);
+t_u16	recognize(char **block);
+t_u16	translate(char **block, int xshift, int yshift);
+char	**chunk(char **block, t_point pos);
+void	set_chunk(char **block, char **chunk, t_point pos);
+void	merge_chunk(char **tet, char **cur);
+int		col_empty(char **block, int l, int n);
+int		line_empty(char **block, int col, int n);
 
 #endif
